93511cbca51036b7fdc67a972600bbe6e88861d3
cxrvus <benito.m.rabe@gmail.com>
2024-09-02 18:31:10 +0200

    progress with import functionality

---
+++ src/import.rs
@@ -0,0 +1,54 @@
+use std::path::PathBuf;
+use anyhow::{Context, Result};
+use csv;
+use serde::{Deserialize, Serialize};
+use serde_json::{Value};
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Transaction {
+	date: String,
+	amount: f64,
+	description: String,
+}
+
+pub fn import_transactions (path: PathBuf) -> Result<()> {
+	let t = convert_transactions(path);
+	print!("{:?}", t);
+	Ok(())
+}
+
+
+pub fn convert_transactions (path: PathBuf) -> Result<Vec<Transaction>> { 
+	let mut rdr = csv::ReaderBuilder::new()
+		.delimiter(b';')
+		.from_path(path)?;
+	let transactions = rdr.deserialize();
+	let mut simple_transactions: Vec<Transaction> = vec![];
+
+	for transaction in transactions {
+		// todo: parse to serde first, then convert
+		// todo: also refactor to make convert_transactions pure
+		// todo: add anyhow contexts (here and in main)
+
+		let transaction: Value = transaction.context("failed to parse transaction")?;
+		print!("{:?}", transaction);
+		let transaction = transaction.as_object().expect("failed to convert CSV object to JSON object");
+
+		let date = transaction.get("Buchungstag").expect("raw transaction is missing required field 'Buchungstag'");
+		let date = date.as_str().unwrap_or_default().to_owned();
+		let amount = transaction.get("Betrag").expect("raw transaction is missing required field 'Betrag'");
+		let amount = amount.as_f64().unwrap_or_default();
+
+		let description = ["Buchungstext", "Verwendungszweck", "Beguenstigter/Zahlungspflichtiger"]
+		 	.into_iter()
+			.map(|field| transaction.get(field).unwrap_or(&Value::Null).as_str().unwrap())
+			.collect::<Vec<&str>>()
+			.join(";")
+		;
+
+		let simple_transaction = Transaction { date, amount, description };
+		simple_transactions.push(simple_transaction);
+	}
+
+	Ok(simple_transactions)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use clap::Parser;
+use std::path::PathBuf;
+use anyhow::Result;
+mod import;
+
 fn main() {
-    println!("Hello World!");
+	match execute() {
+		Ok(_) => {},
+		Err(e) => println!("<!>\n{:?}", e)
+	}
+}
+
+fn execute() -> Result<()> {
+	let res = Cli::parse();
+	match res {
+		Cli::Import(args) => {
+			import::import_transactions(args.path)
+		}
+	}
+}
+
+#[derive(Parser)]
+#[clap(version, about)]
+enum Cli {
+	Import(ImportArgs),
+}
+
+#[derive(Parser)]
+struct ImportArgs {
+	#[arg(required = true)]
+	path: PathBuf,
 }
