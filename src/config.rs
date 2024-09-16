bee85a16cf1ceff2ff200d00ba30391dc3bf365d
cxrvus <benito.m.rabe@gmail.com>
2024-09-16 23:48:53 +0200

    minor config.rs progress

--- src/config.rs
+++ src/config.rs
@@ -1,5 +1,21 @@
-use std::fs::read;
+use std::{fs::read, path::Path};
+use serde::Deserialize;
+
+pub const CFG_PATH: &'static str = "~/cxconfig.json";
+
+#[derive(Deserialize)]
+pub struct Config {
+	db_root: Path
+}
+
+impl Default for Config {
+	fn default() -> Self {
+		Self {
+			db_root: "./db"
+		}
+	}
+}
 
 pub fn get_config() {
-	
+	todo!("get config file")
 }
