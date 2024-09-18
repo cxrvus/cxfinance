ff1d2add9d96be49c392500e469dd41f80069f36
cxrvus <benito.m.rabe@gmail.com>
2024-09-18 10:38:26 +0200

    getting db_root from config file

--- src/config.rs
+++ src/config.rs
@@ -1,21 +1,31 @@
-use std::{fs::read, path::Path};
+use anyhow::Result;
+use std::{fs::read_to_string, path::{Path, PathBuf}};
 use serde::Deserialize;
+use serde_json;
 
-pub const CFG_PATH: &'static str = "~/cxconfig.json";
+const CFG_PATH: &'static str = "~/cxconfig.json";
 
 #[derive(Deserialize)]
 pub struct Config {
-	db_root: Path
+	pub db_root: PathBuf
 }
 
 impl Default for Config {
 	fn default() -> Self {
 		Self {
-			db_root: "./db"
+			db_root: PathBuf::from("./db")
 		}
 	}
 }
 
-pub fn get_config() {
-	todo!("get config file")
+pub fn get_config() -> Result<Config> {
+	let path = Path::new(CFG_PATH);
+	if !path.exists() {
+		println!("Config file not found, using default settings.\ncreate a {} file in your home directory.", CFG_PATH);
+		return Ok(Config::default())
+	}
+	else {
+		let string = read_to_string(path)?;
+		return serde_json::from_str(&string).map_err(|e| anyhow::Error::new(e));
+	}
 }
