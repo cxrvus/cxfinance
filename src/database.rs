16c6dcaabbdb511760f9cf6a68c8230271419fc6
cxrvus <benito.m.rabe@gmail.com>
2024-09-23 01:25:47 +0200

    misc Database

--- src/database.rs
+++ src/database.rs
@@ -9,7 +9,7 @@ use serde::{de::DeserializeOwned, Serialize};
 use crate::config::get_config;
 
 pub struct Database<T> {
-	file_path: PathBuf,
+	path: PathBuf,
 	pub records: Vec<T>,
 }
 
@@ -18,30 +18,35 @@ impl<T: DeserializeOwned> Database<T> {
 		let folder_path = get_config()?.db_root.clone();
 
 		if !folder_path.exists() {
-			create_dir(&folder_path).context("failed to create database folder")?;
+			create_dir(&folder_path).expect("failed to create database folder");
 			println!(
 				"created new database folder at {}",
 				folder_path.to_str().unwrap()
 			);
 		}
 
+		let file_path = PathBuf::from(file_path);
 		let mut full_path = folder_path.clone();
 		full_path.push(&file_path);
 
+		if let None = file_path.extension() {
+			panic!("specified database file is missing a file extension\n(needs to be JSON or CSV)");
+		}
+
 		if !full_path.exists() {
-			write(&full_path, "[]\n").context("failed to create empty database")?;
+			write(&full_path, "[]\n").expect("failed to create empty database");
 			println!(
 				"created new database file {}",
 				full_path.file_name().unwrap().to_str().unwrap()
 			);
 		}
 
-		let db_content = read_to_string(&full_path).context("failed to read database file")?;
+		let db_content = read_to_string(&full_path).expect("failed to read database file");
 		let records: Vec<T> =
 			serde_json::from_str(&db_content).context("failed to parse database")?;
 
 		Ok(Self {
-			file_path: full_path,
+			path: full_path,
 			records,
 		})
 	}
@@ -50,7 +55,7 @@ impl<T: DeserializeOwned> Database<T> {
 impl<T: Serialize> Database<T> {
 	pub fn save(&self) -> Result<()> {
 		let stringified = serde_json::to_string_pretty(&self.records)?;
-		write(&self.file_path, stringified).context("failed to write to database file")?;
+		write(&self.path, stringified).context("failed to write to database file")?;
 		Ok(())
 	}
 }
